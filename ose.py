6d890f282eefb7dd1fe83d6a09e602d7152799d6
koukis <koukis@upstreamsystems.com>
2016-09-13 14:46:14 +0300

    Show stopped trains in red

--- ose.py
+++ ose.py
@@ -165,7 +165,7 @@ def trainsToDisplay(trains):
         for train in trains:
             train = train[1]
             if train['station'] == station_name:
-                stations[station] = stations[station] + str(train['trip_id']) + " (" +str(train['distance']) + " km) "
+                stations[station] = (stations[station] + str(train['trip_id']) + " (" +str(train['distance']) + " km) ", float(train['speed'].replace(",",".")))
 
     return stations
 
