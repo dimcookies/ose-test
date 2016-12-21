25d763ac9664dff6f1afd84ea3c07c626339bc78
koukis <dimcookies@gmail.com>
2016-12-21 10:26:13 +0200

    Add previous distance for oasa

--- transport/oasa/oasa.py
+++ transport/oasa/oasa.py
@@ -1,5 +1,7 @@
 from oasa_data import  *
 from transport.utils import *
+from flask import session
+
 
 def stopsData():
 
@@ -18,7 +20,7 @@ def stopsData():
     for route in routes.keys():
         res = []
         for bus in getWSJsonResponse(OASA_WS_BUS_LOCATION % route):
-            res.append((float(bus['CS_LAT']), float(bus['CS_LNG'])))
+            res.append((float(bus['CS_LAT']), float(bus['CS_LNG']),bus['VEH_NO']))
         buses[route] = res
 
     for stop in stops:
@@ -26,7 +28,15 @@ def stopsData():
         for route in stop['routes']:
             for bus in buses[route]:
                 #print route,lines
-                res.append({'route':routes[route], 'dist':round(distance_on_unit_sphere(bus[0], bus[1], stop['stop_lat'], stop['stop_lng']) ,2)})
+                dist = round(distance_on_unit_sphere(bus[0], bus[1], stop['stop_lat'], stop['stop_lng']), 2)
+                session_key =  str(route)+ "_" + bus[2]
+                prev_dist = ""
+                diff = 0
+                if session.has_key(session_key):
+                    prev_dist = session[session_key]
+                    diff = dist - prev_dist
+                session[session_key] = dist
+                res.append({'route':routes[route], 'veh_code':bus[2], 'dist':dist, 'prev_dist': prev_dist, 'diff': diff})
         stop['buses'] = res
 
     return stops
